223beed40f2d6a68451e1c939239d1bd4572e2a4
AleMoorgan <aangelresendizhdz@gmail.com>
2025-12-01 20:00:24 +0000

    Corregir test_basic para usar src.interpreter

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,9 @@
-from interpreter import evaluar, interpretar
+import src.interpreter as interpreter
+
 def test_evaluar_entero():
     # Limpiamos variables globales antes de probar
     interpreter.variables.clear()
     interpreter.variables['x'] = 5
     result = interpreter.evaluar('x + 3')
     assert result == 8
+
